fc213df8492cc20177741b638aa0d26a0f8b1c45
dipakbhise <dipakbhise420@gmail.com>
2024-05-04 17:17:44 +0530

    refactor: nextjs configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        scrollRestoration: true, // if user click on particular job post and then come back it will go on that particular job post
+      },
+    
+        reactStrictMode: false, // if 'true' it renders every component two times
+};
 
 export default nextConfig;
