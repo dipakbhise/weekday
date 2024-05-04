6dbf420a0beba1a724ff0335ee43b808ef14ed84
dipakbhise <dipakbhise420@gmail.com>
2024-05-04 20:00:49 +0530

    feat: wrap redux store provider to access redux in whole app

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import StoreProvider from "@/redux/StoreProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +11,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    // wrap redux storeprovider to access redux in wholw app
+    <StoreProvider>
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
+    </StoreProvider>
   );
 }
