756d57977d644d2761e1d83d45bd9154e38bada2
dipakbhise <dipakbhise420@gmail.com>
2024-05-04 16:49:12 +0530

    feat: add common function to fetch api's

---
+++ utils/methods.js
@@ -0,0 +1,42 @@
+// common function to call api with parameters like api endpoint, method, payload,additionalHeaders, successfunction which will call after api response
+
+export const fetchApi = async (
+  endpoint,
+  method = "GET",
+  payload = null,
+  additionalHeaders = {},
+  successFunc = null
+) => {
+  const url = `${BASE_URL}${endpoint}`;
+  const headers = {
+    "Content-Type": "application/json",
+    ...additionalHeaders,
+    // Add any other headers you need (e.g., authorization tokens)
+  };
+
+  const options = {
+    method,
+    headers,
+    body: payload ? JSON.stringify(payload) : null,
+  };
+
+  try {
+    // fetch is the vanila js function for AJAX calls
+    const response = await fetch(url, options);
+    if (response.ok) {
+
+    // call the success func after api success response 
+      if (successFunc) {
+        successFunc();
+      }
+    } else {
+      console.error("API request failed");
+    }
+
+    // convert the fetched data into json format and return it
+    return response.json();
+  } catch (error) {
+    console.error("API request failed:", error.message);
+    throw error;
+  }
+};
