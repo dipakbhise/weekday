d828ba3eca9e681e0bc4dcd3636d27a8e2945845
dipakbhise <dipakbhise420@gmail.com>
2024-05-04 16:46:50 +0530

    feat: added base api url for global use

---
+++ utils/config.js
@@ -0,0 +1,2 @@
+// Backend API base url to use across the project if in futur the api base url changes then we can change it from only one place
+export const BASE_URL = "https://api.weekday.technology"
