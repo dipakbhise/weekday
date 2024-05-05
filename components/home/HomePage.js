2dc236819543c26ea5defda4ac3b5e431d7e50db
dipakbhise <dipakbhise420@gmail.com>
2024-05-05 22:14:49 +0530

    feat: implemented filter function with redux to persist data

--- components/home/HomePage.js
+++ components/home/HomePage.js
@@ -2,6 +2,7 @@
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import {
   selectJobPostsDetails,
+  setFilteredJobPosts,
   setJobPostsDetails,
 } from "@/redux/slices/jobListingSlice";
 import { jobList } from "@/utils/apiEndpoints";
@@ -24,10 +25,10 @@ const HomePage = (props) => {
 
   useEffect(() => {
     // fetch joblisting from api at initial time when their is no data
-    jobPostsDetails.jobPosts.length === 0 && fetchJobLists(100, 0);
+    jobPostsDetails.jobPosts.length === 0 && fetchJobLists(100, 0, true);
   }, []);
 
-  const fetchJobLists = async (limit, offset) => {
+  const fetchJobLists = async (limit, offset, initial) => {
     setLoading(true);
     try {
       const payload = {
@@ -44,9 +45,26 @@ const HomePage = (props) => {
       };
 
       // update the joblistings data
-
       dispatch(setJobPostsDetails(postListingDetails));
+
+      // roleFilter, experienceFilter,remoteFilter,locationFilter,basepayFilter,companyNameFilter
+
+      console.log(
+        "jobPostsDetails.ReduxSelectedRoleOptions",
+        jobPostsDetails.ReduxSelectedRoleOptions
+      );
+
+      !initial &&
+        filterFunction(
+          jobPostsDetails.ReduxSelectedRoleOptions,
+          jobPostsDetails.ReduxSelectedExperience,
+          jobPostsDetails.ReduxSelectedRemoteOptions,
+          jobPostsDetails.ReduxSelectedLocationOptions,
+          jobPostsDetails.ReduxSelectedBasePay,
+          jobPostsDetails.ReduxCompanyName
+        );
       setLoading(false);
+      console.log("jobPostsDetails.jobPosts", jobPostsDetails.jobPosts.length);
     } catch (error) {
       console.error(error);
     }
@@ -75,17 +93,112 @@ const HomePage = (props) => {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [jobPostsDetails.totalCount, jobPostsDetails.jobPosts, loading]);
+  }, [
+    jobPostsDetails.totalCount,
+    jobPostsDetails.jobPosts,
+    loading,
+    jobPostsDetails.ReduxSelectedRoleOptions,
+    jobPostsDetails.ReduxSelectedRemoteOptions,
+    jobPostsDetails.ReduxSelectedLocationOptions,
+    jobPostsDetails.ReduxSelectedExperience,
+    jobPostsDetails.ReduxSelectedBasePay,
+    jobPostsDetails.ReduxCompanyName
+  ]);
+
+  // Define a function to filter the job posts
+  const filterJobPosts = (selectedFilters, companyNameFilter) => {
+    console.log("selectedFilters", selectedFilters);
+    const filteredData = jobPostsDetails.jobPosts.filter((jobPost) => {
+      // Check if each job post matches all the selected filters and company name filter
+
+      // Check role filter
+      const roleMatch =
+        selectedFilters.roleFilter.length === 0 ||
+        selectedFilters.roleFilter.some(
+          (role) => role.name.toLowerCase() === jobPost.jobRole.toLowerCase()
+        );
+
+      // Check remote filter
+      const remoteMatch =
+        selectedFilters.remoteFilter.length === 0 ||
+        selectedFilters.remoteFilter.some(
+          (remote) =>
+            remote.name.toLowerCase() === jobPost.location.toLowerCase()
+        );
+
+      // Check location filter
+      const locationMatch =
+        selectedFilters.locationFilter.length === 0 ||
+        selectedFilters.locationFilter.some(
+          (location) =>
+            location.name.toLowerCase() === jobPost.location.toLowerCase()
+        );
+
+      // Check company name filter
+      const companyMatch =
+        companyNameFilter === "" ||
+        jobPost.companyName
+          .toLowerCase()
+          .includes(companyNameFilter.toLowerCase());
+
+
+    // Check experience filter
+    const experienceMatch = !selectedFilters.experienceFilter.name || 
+      (Number(selectedFilters.experienceFilter.name) >= jobPost.minExp && 
+       Number(selectedFilters.experienceFilter.name) <= jobPost.maxExp);
+
+
+      // Check basepay filter
+      const basepayMatch =
+        !selectedFilters.basepayFilter.name ||
+        selectedFilters.basepayFilter.name <= jobPost.minJdSalary;
+
+      // Check if any of the selected options match the corresponding property of the job post
+      return (
+        roleMatch &&
+        remoteMatch &&
+        locationMatch &&
+        companyMatch &&
+        experienceMatch &&
+        basepayMatch
+      );
+    });
+
+    console.log("filteredData", filteredData);
+
+    const data = { jobPosts: filteredData };
+
+    dispatch(setFilteredJobPosts(data));
+  };
+
+  const filterFunction = (
+    roleFilter = [],
+    experienceFilter = {},
+    remoteFilter = [],
+    locationFilter = [],
+    basepayFilter = {},
+    companyNameFilter = ""
+  ) => {
+    const selectedFilters = {
+      roleFilter: roleFilter, // Example of selected role filter
+      experienceFilter: experienceFilter, // Example of selected experience filter
+      remoteFilter: remoteFilter, // Example of selected remote filter
+      locationFilter: locationFilter, // Example of selected location filter
+      basepayFilter: basepayFilter, // Example of selected basepay filter
+    };
+
+    filterJobPosts(selectedFilters, companyNameFilter);
+  };
 
   return (
     <>
-      <Filters />
+      <Filters filterJobPosts={filterJobPosts} />
       <div className="post-card-container">
-        {jobPostsDetails.jobPosts.length > 0
-          ? jobPostsDetails.jobPosts.map((jobPost, index) => (
+        {jobPostsDetails.filteredJobPosts.length > 0
+          ? jobPostsDetails.filteredJobPosts.map((jobPost, index) => (
               <JobCard post={jobPost} key={index} />
             ))
-          : jobPostsDetails.jobPosts.length < 10
+          : jobPostsDetails.filteredJobPosts.length < 10
           ? Array.from({ length: 10 }).map((_, index) => (
               <div key={index} style={{ margin: "10px 0px" }}>
                 <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
