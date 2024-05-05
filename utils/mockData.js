79462832dbecad61cf3f22421d0ddbcfb546c5d2
dipakbhise <dipakbhise420@gmail.com>
2024-05-05 18:37:52 +0530

    feat:added mockdata for filters dropdown

---
+++ utils/mockData.js
@@ -0,0 +1,77 @@
+export const roleFilters = [
+  {
+    group: "Engineering",
+    id: 1,
+    roles: [
+      { id: 1, name: "Backend" },
+      { id: 2, name: "Frontend" },
+      { id: 3, name: "Fullstack" },
+      { id: 4, name: "IOS" },
+      { id: 5, name: "Flutter" },
+      { id: 6, name: "React Native" },
+      { id: 7, name: "Android" },
+      { id: 8, name: "Tech Lead" },
+      { id: 9, name: "Dev-Ops" },
+      { id: 10, name: "Data Engineer" },
+      { id: 11, name: "Data Science" },
+      { id: 12, name: "Data Science" },
+    ],
+  },
+  {
+    group: "Design",
+    id: 2,
+    roles: [
+      { id: 13, name: "Designer" },
+      { id: 14, name: "Design Manager" },
+      //   { id: 13, name: "Frontend" },
+      //   { id: 14, name: "Frontend" },
+      //   { id: 15, name: "Frontend" },
+      //   { id: 16, name: "Frontend" },
+      //   { id: 17, name: "Frontend" },
+      //   { id: 18, name: "Frontend" },
+      //   { id: 19, name: "Frontend" }
+    ],
+  },
+];
+
+export const experienceFilter = [
+  { id: 1, name: "1" },
+  { id: 2, name: "2" },
+  { id: 3, name: "3" },
+  { id: 4, name: "4" },
+  { id: 5, name: "5" },
+  { id: 6, name: "6" },
+  { id: 7, name: "7" },
+  { id: 7, name: "8" },
+  { id: 7, name: "9" },
+  { id: 7, name: "10" },
+];
+
+export const remoteFilter = [
+  { id: 1, name: "Remote" },
+  { id: 2, name: "Hybrid" },
+  { id: 3, name: "In-Office" },
+];
+
+export const locationFilter = [
+    { id: 1, name: "Delhi" },
+    { id: 2, name: "Mumbai" },
+    { id: 3, name: "Bangaluru" },
+  ];
+
+  export const basepayFilter = [
+    { id: 1, name: "10 LPA" },
+    { id: 2, name: "20 LPA" },
+    { id: 3, name: "30 LPA" },
+  ];
+
+  export const employeeFilter = [
+    { id: 1, name: "1-10" },
+    { id: 2, name: "11-20" },
+    { id: 3, name: "21-50" },
+    { id: 4, name: "51-100" },
+    { id: 5, name: "101-200" },
+    { id: 6, name: "201-500" },
+    { id: 7, name: "500+" },
+  ];
+
