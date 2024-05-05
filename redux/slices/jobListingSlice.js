222f53fba241e6b301233f382c88a7c35b6d4f7a
dipakbhise <dipakbhise420@gmail.com>
2024-05-05 22:15:40 +0530

    feat: added reducers and actions for filters to persist data

--- redux/slices/jobListingSlice.js
+++ redux/slices/jobListingSlice.js
@@ -5,6 +5,13 @@ const initialState = {
   pageNumber:0,
   jobPostsPerPage:10,
   totalCount:0,
+  filteredJobPosts:[],
+  ReduxSelectedRoleOptions:[],
+  ReduxSelectedRemoteOptions:[],
+  ReduxSelectedLocationOptions:[],
+  ReduxSelectedBasePay:{},
+  ReduxSelectedExperience:{},
+  ReduxCompanyName:""
 };
 
 export const jobListingSlice = createSlice({
@@ -16,12 +23,34 @@ export const jobListingSlice = createSlice({
       state.jobPosts = [...state.jobPosts,...action.payload.jobPosts];
       state.pageNumber = action.payload.pageNumber,
       state.jobPostsPerPage = action.payload.jobPostsPerPage,
-      state.totalCount = action.payload.totalCount
+      state.totalCount = action.payload.totalCount,
+      state.filteredJobPosts = [...state.jobPosts,...action.payload.jobPosts];
+    },
+    setFilteredJobPosts(state, action) {
+      state.filteredJobPosts = [...action.payload.jobPosts];
+    },
+    setReduxSelectedRoleOptions(state, action) {
+      state.ReduxSelectedRoleOptions = [...action.payload];
+    },
+    setReduxSelectedRemoteOptions(state, action) {
+      state.ReduxSelectedRemoteOptions = [...action.payload];
+    },
+    setReduxSelectedLocationOptions(state, action) {
+      state.ReduxSelectedLocationOptions = [...action.payload];
+    },
+    setReduxSelectedBasePay(state, action) {
+      state.ReduxSelectedBasePay = action.payload;
+    },
+    setReduxSelectedExperience(state, action) {
+      state.ReduxSelectedExperience = action.payload;
+    },
+    setReduxCompanyName(state, action) {
+      state.ReduxCompanyName = action.payload;
     },
 
   },
 });
 
-export const { setJobPostsDetails } = jobListingSlice.actions; // export actions
+export const { setJobPostsDetails,setFilteredJobPosts,setReduxSelectedRoleOptions,setReduxSelectedRemoteOptions,setReduxSelectedLocationOptions,setReduxSelectedBasePay,setReduxSelectedExperience,setReduxCompanyName } = jobListingSlice.actions; // export actions
 export const selectJobPostsDetails = (state) => state.jobListing; // directly written useSelector's callback here and exported it. 'auth' is an name of all reducers of authSlice
 export default jobListingSlice.reducer; // exporting all reducers
