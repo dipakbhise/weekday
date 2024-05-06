b38d7981dbb6b07416d1be974f4cc0eedb7d036c
dipakbhise <dipakbhise420@gmail.com>
2024-05-06 10:04:36 +0530

    feat: add lexend font

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
-import { Inter } from "next/font/google";
+import { Inter, Lexend } from "next/font/google";
 import "./globals.css";
 import StoreProvider from "@/redux/StoreProvider";
 
 const inter = Inter({ subsets: ["latin"] });
+const lexend = Lexend({ subsets: ["latin"],weight: ['100','200','300','400','500','600','700','900'] });
 
 export const metadata = {
   title: "Weekday - Hire Engineers Vouched by other techies",
@@ -14,7 +15,7 @@ export default function RootLayout({ children }) {
     // wrap redux storeprovider to access redux in wholw app
     <StoreProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={lexend.className}>{children}</body>
     </html>
     </StoreProvider>
   );
